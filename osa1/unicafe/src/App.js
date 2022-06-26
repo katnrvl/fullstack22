73892b90e0e52e3f8e84df13ffcdd835b5203fef
AB5225 <ab5225@student.jamk.fi>
2022-06-26 21:18:25 +0300

    1.6 done

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -6,9 +6,32 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const Button = (props) => (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+
+  const setToValue = (value, newValue) => {
+    value(newValue)
+  }
+
+  const Stats = () => {
+    return ( 
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+    )
+  }
+
   return (
     <div>
-      * CODE HERE *
+      <h1>leave feedback</h1>
+      <Button handleClick={() => setToValue(setGood, good + 1)} text="good" />
+      <Button handleClick={() => setToValue(setNeutral, neutral + 1)} text="neutral" />
+      <Button handleClick={() => setToValue(setBad, bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <Stats />
     </div>
   )
 }

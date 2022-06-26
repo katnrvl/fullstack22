b571526387e3a90f39c95bde8a88fcdadfc56d8b
AB5225 <ab5225@student.jamk.fi>
2022-06-26 21:36:21 +0300

    1.8 done

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,30 +1,32 @@
 import { useState } from 'react'
 
+const Statistics = () => {
+  return (
+    <div>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {good + neutral + bad}</p>
+    <p>average {(good + (bad * -1)) / (good + neutral + bad)}</p>
+    <p>positive {good / (good + neutral + bad)} %</p>
+    </div>
+  )
+}
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const Button = (props) => (
-    <button onClick={props.handleClick}>{props.text}</button>
-  )
+
 
   const setToValue = (value, newValue) => {
     value(newValue)
-  }
-
-  const Stats = () => {
-    return ( 
-      <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + neutral + bad}</p>
-        <p>average {(good + (bad * -1)) / (good + neutral + bad)}</p>
-        <p>positive {good / (good + neutral + bad)} %</p>
-      </div>
-    )
   }
 
   return (
@@ -33,7 +35,7 @@ const App = () => {
       <Button handleClick={() => setToValue(setGood, good + 1)} text="good" />
       <Button handleClick={() => setToValue(setNeutral, neutral + 1)} text="neutral" />
       <Button handleClick={() => setToValue(setBad, bad + 1)} text="bad" />
-      <h1>statistics</h1>
+      <Statistics />
       <Stats />
     </div>
   )
